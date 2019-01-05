a0a91136b01e5a70666f472007d0ebab233f5b96
Nick Bansal <https://github.com/NickBansal>
2019-01-05 18:43:11 +0000

    Hamburger added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import './Stylesheets/App.css';
 import Navbar from './Components/Navbar'
-
+import About from './Components/About'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Navbar />
+        <About />
       </div>
     );
   }
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -6,23 +6,32 @@ import '../Stylesheets/Navbar.css'
 const Navbar = () => {
     const style = {
         backgroundImage: `url(${Mountain})`,
-        minHeight: '80vh',
+        minHeight: '85vh',
         top: '0',
         left: '0',
         width: '100%',
         height: '100%',
         backgroundSize: 'cover',
-        zIndex: '-1'
+        zIndex: '-1',
+        backgroundPosition: 'center'
     }
     return (
         <div style={style} className="TitlePage">
             <nav>
-                <h1>Nick Bansal</h1>
+                <h1 className="Name">Nick Bansal</h1>
+                <h1 className="Job">Full Stack Developer</h1>
                 <ul className="NavLinks">
                     <li>About</li>
                     <li>Projects</li>
-                    <li>Links</li>
+                    <li>Contact</li>
                 </ul>
+                <div className="Hamburger">
+                    <div className="burger-menu">
+                        <div className="bar1" key="b1" />
+                        <div className="bar2" key="b2" />
+                        <div className="bar3" key="b3" />
+                    </div>
+                </div>
             </nav>
             <h1 id="MeJob">Full Stack Developer</h1>
             <img id="MePic" src={Me} alt="Me"/>
