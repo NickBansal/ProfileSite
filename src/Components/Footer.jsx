a736edf5528950bbe150f7e06c70b05b059a7a3a
Nick Bansal <https://github.com/NickBansal>
2019-01-06 18:26:05 +0000

    Skills added

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,7 +4,8 @@ import '../Stylesheets/Footer.css'
 const Footer = () => {
     return (
         <div className="Footer">
-            <h1>Footer</h1>
+            <h4>Email: bansal32182@gmail.com</h4>
+            <h4>© 2019 Copyright - Nick Bansal</h4>
         </div>
     )
 }
