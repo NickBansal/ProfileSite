78999e3d63fb55c89173fa4e332b179d1887b395
Nick Bansal <https://github.com/NickBansal>
2019-01-06 16:15:15 +0000

    About me page updated

--- src/Components/IconLinks.jsx
+++ src/Components/IconLinks.jsx
@@ -8,23 +8,23 @@ const IconLinks = () => {
                 rel="noopener noreferrer"
                 target="_blank"
                 href="https://github.com/NickBansal">
-                <i className="fab fa-github fa-3x"></i></a>
+                <i className="i-link fab fa-github fa-3x"></i></a>
             <a
                 rel="noopener noreferrer"
                 target="_blank"
                 href="https://stackoverflow.com/users/10775236/nick-bansal">
-                <i className="fab fa-stack-overflow fa-3x"></i></a>
+                <i className="i-link fab fa-stack-overflow fa-3x"></i></a>
             <a
                 rel="noopener noreferrer"
                 target="_blank"
                 href="https://codepen.io/NickyBee/">
-                <i className="fab fa-codepen fa-3x"></i>
+                <i className="i-link fab fa-codepen fa-3x"></i>
             </a>
             <a
                 rel="noopener noreferrer"
                 target="_blank"
                 href="https://www.codewars.com/users/NickBansal">
-                <i className="fas fa-code fa-3x"></i>
+                <i className="i-link fas fa-code fa-3x"></i>
             </a>
         </div>
     )
--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -7,6 +7,24 @@ const About = () => {
         <div>
             <div id="About">
                 <h1>About</h1>
+                    <p className="part1">A Javascript enthusiast</p>
+                <div className="AboutParagraph">
+                    <br />
+                    <p className="part2"> I'm a full stack developer who has recently graduated from an intensive coding bootcamp at Northcoders, 
+                        looking to continue a career within a creative and technical environment.</p>
+                    <br />
+                    <br />
+                    <p className="part3">As well as obtaining a wealth of coding knowledge I have also learnt a 
+                        lot of vital skills whilst on this bootcamp such as 
+                        Test Driven development, Pair programming and Functional Programming</p>
+                    <div className="iconSkills">
+                        <i className="i-skill fab fa-js fa-3x"></i>
+                        <i className="i-skill fab fa-react fa-3x"></i>
+                        <i className="i-skill fas fa-heart fa-4x"></i>
+                        <i className="i-skill fab fa-node-js fa-3x"></i>
+                        <i className="i-skill fab fa-css3 fa-3x"></i>
+                    </div>
+                </div>
             </div>
             <DownArrow />
         </div>
