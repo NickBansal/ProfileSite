b7a28d85f7f04880d9b8eeaec45a2eb1eca5865c
Nick Bansal <https://github.com/NickBansal>
2019-01-06 10:47:39 +0000

    Icon links now working

--- src/Components/Landing.jsx
+++ src/Components/Landing.jsx
@@ -43,10 +43,28 @@ class Navbar extends Component {
                 <h3>Author, DreamWeaver, Visionary...</h3>
                 <h1 id="MeJob">Full Stack Developer</h1>
                 <div className="ContactIcons">
-                    <i class="fab fa-github fa-3x"></i>
-                    <i class="fab fa-stack-overflow fa-3x"></i>
+                    <a 
+                    rel="noopener noreferrer"  
+                    target="_blank" 
+                    href="https://github.com/NickBansal">
+                    <i class="fab fa-github fa-3x"></i></a>
+                    <a 
+                    rel="noopener noreferrer"  
+                    target="_blank" 
+                    href="https://stackoverflow.com/users/10775236/nick-bansal">
+                    <i class="fab fa-stack-overflow fa-3x"></i></a>
+                    <a 
+                    rel="noopener noreferrer"  
+                    target="_blank" 
+                    href="https://codepen.io/NickyBee/">
                     <i class="fab fa-codepen fa-3x"></i>
+                    </a>
+                    <a 
+                    rel="noopener noreferrer"  
+                    target="_blank" 
+                    href="https://www.codewars.com/users/NickBansal">
                     <i class="fas fa-code fa-3x"></i>
+                    </a>
                 </div>
                 <img id="MePic" src={Me} alt="Me"/>
             </div>
