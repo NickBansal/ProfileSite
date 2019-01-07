749b4f0735712704bbd0c923bc94e2b1eb46bb64
Nick Bansal <https://github.com/NickBansal>
2019-01-07 11:28:55 +0000

    NC news animation updated

--- src/Components/NCProject.jsx
+++ src/Components/NCProject.jsx
@@ -7,7 +7,33 @@ const NCProject = () => {
     <div className="hvrbox">
         <img src={NC} alt="JS" className="hvrbox-layer_bottom" />
         <div className="hvrbox-layer_top hvrbox-layer_scale">
-            <div className="hvrbox-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce porttitor ligula porttitor, lacinia sapien non.</div>
+            <div className="hvrbox-text">
+                <h2>NC Comic-News</h2>
+                <br/>
+                <p>ComicNews is a news aggregation demo site built in React.js</p>
+                <br/>
+                <p>I created both front end and back end of this project and the links can be found below</p>
+                <br/>
+                <a
+                rel="noopener noreferrer"
+                target="_blank"
+                href="http://evening-plains-23561.herokuapp.com/">WebSite
+                </a>
+                <br/>
+                <br/>
+                <a
+                rel="noopener noreferrer"
+                target="_blank"
+                href="https://github.com/NickBansal/Comic-News">Github - FrontEnd
+                </a>
+                <br/>
+                <br/>
+                <a
+                rel="noopener noreferrer"
+                target="_blank"
+                href="https://github.com/NickBansal/Comic-News-Backend">Github - BackEnd
+                </a>
+            </div>
         </div>
     </div>
     )
