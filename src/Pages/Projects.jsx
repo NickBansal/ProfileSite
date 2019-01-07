6ce08f186868f0d4740d38f440c6f3c6978555f7
Nick Bansal <https://github.com/NickBansal>
2019-01-07 09:23:05 +0000

    Project pictures added

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import '../Stylesheets/Projects.css'
 import Slider from "react-slick";
 import NCProfile from '../Stylesheets/Images/NC.png'
+import p5 from '../Stylesheets/Images/p5.png'
+import JS from '../Stylesheets/Images/JS.png'
 
 const Projects = () => {
     
@@ -13,16 +15,6 @@ const Projects = () => {
         slidesToScroll: 1
     };
 
-    const style = {
-        backgroundImage: `url(${NCProfile})`,
-        top: '0',
-        left: '0',
-        backgroundSize: '100% 99.9%',
-        backgroundRepeat: 'none',
-        zIndex: '-2',
-        backgroundPosition: 'center'
-    }
-
     return (
         <div id="Projects">
             <h1>Projects</h1>
@@ -30,16 +22,21 @@ const Projects = () => {
             <Slider {...settings}>
                 <div>
                     <div 
-                    style={style}
+                    style={{ backgroundImage: `url(${NCProfile})` }}
                     className="ProjectItem nc-profile">
-                        {/* <h3>Northcoders Projects Site</h3> */}
                     </div>
                 </div>
                 <div>
-                <div className="ProjectItem"><h3>JS Games</h3></div>
+                    <div 
+                    style={{ backgroundImage: `url(${p5})` }}
+                    className="ProjectItem">
+                    </div>
                 </div>
                 <div>
-                <div className="ProjectItem"><h3>p5.js</h3></div>
+                    <div 
+                    style={{ backgroundImage: `url(${JS})` }}
+                    className="ProjectItem">
+                    </div>
                 </div>
             </Slider>
             </div>
