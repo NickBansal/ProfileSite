4349c6f3dd2eceeb11dedee70a0e2c02ce58a08f
Nick Bansal <https://github.com/NickBansal>
2019-01-07 10:50:09 +0000

    Project pictures animation added

--- src/Components/JSProjects.jsx
+++ src/Components/JSProjects.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import '../Stylesheets/JSProjects.css'
+import '../Stylesheets/ProjectItems.css'
 import JS from '../Stylesheets/Images/JS.png'
 
 const JSProjects = () => {
     return (
     <div className="hvrbox">
         <img src={JS} alt="JS" className="hvrbox-layer_bottom" />
-        <div className="hvrbox-layer_top">
+        <div className="hvrbox-layer_top hvrbox-layer_scale">
             <div className="hvrbox-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce porttitor ligula porttitor, lacinia sapien non.</div>
         </div>
     </div>
--- src/Components/NCProject.jsx
+++ src/Components/NCProject.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import '../Stylesheets/JSProjects.css'
+import '../Stylesheets/ProjectItems.css'
 import NC from '../Stylesheets/Images/NC.png'
 
 const NCProject = () => {
     return (
     <div className="hvrbox">
         <img src={NC} alt="JS" className="hvrbox-layer_bottom" />
-        <div className="hvrbox-layer_top">
+        <div className="hvrbox-layer_top hvrbox-layer_scale">
             <div className="hvrbox-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce porttitor ligula porttitor, lacinia sapien non.</div>
         </div>
     </div>
