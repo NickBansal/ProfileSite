420c5f79ef4f933fd2bf4d1dc3549eb0135e6d32
Nick Bansal <https://github.com/NickBansal>
2019-01-07 09:04:35 +0000

    NC profile added to projects

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -1,19 +1,47 @@
 import React from 'react'
 import '../Stylesheets/Projects.css'
 import Slider from "react-slick";
+import NCProfile from '../Stylesheets/Images/NC.png'
 
 const Projects = () => {
+    
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    };
+
+    const style = {
+        backgroundImage: `url(${NCProfile})`,
+        top: '0',
+        left: '0',
+        backgroundSize: '100% 99.9%',
+        backgroundRepeat: 'none',
+        zIndex: '-2',
+        backgroundPosition: 'center'
+    }
+
     return (
         <div id="Projects">
             <h1>Projects</h1>
             <div className="ProjectFull">
-                <div className="ProjectItem"></div>
-                <div className="ProjectItem"></div>
-                <div className="ProjectItem"></div>
-                <div className="ProjectItem"></div>
-                <div className="ProjectItem"></div>
-                <div className="ProjectItem"></div>
-                <div className="ProjectItem"></div>
+            <Slider {...settings}>
+                <div>
+                    <div 
+                    style={style}
+                    className="ProjectItem nc-profile">
+                        {/* <h3>Northcoders Projects Site</h3> */}
+                    </div>
+                </div>
+                <div>
+                <div className="ProjectItem"><h3>JS Games</h3></div>
+                </div>
+                <div>
+                <div className="ProjectItem"><h3>p5.js</h3></div>
+                </div>
+            </Slider>
             </div>
         </div>
     )
